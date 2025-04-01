66605754d6e0d1946ccca8d20d2c0697a82118f3
NikosGour <nikosgournak@gmail.com>
2025-04-01 09:57:26 +0300

    Project Init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/NikosGour/game_engine
+
+go 1.24.0
---
+++ src/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/NikosGour/game_engine/src/build"
+)
+
+func main(){
+	fmt.Printf("DEBUG_MODE = %t\n",build.DEBUG_MODE)
+}
