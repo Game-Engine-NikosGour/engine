5b1b4322517b9396ea9677e0a50b6826220ac901
NikosGour <nikosgournak@gmail.com>
2025-04-11 18:02:43 +0300

    removed unnecessary code

--- src/lib/platform/windows_os/windows.go
+++ src/lib/platform/windows_os/windows.go
@@ -12,11 +12,6 @@ import (
 	winapi "golang.org/x/sys/windows"
 )
 
-// var (
-// 	_kernel32DLL      windows.Handle
-// 	_GetModuleHandleA uintptr
-// )
-
 type internal_state_windows struct {
 	h_instance winapi.Handle
 	hwnd       winapi.HWND
@@ -24,7 +19,6 @@ type internal_state_windows struct {
 
 func Platform_startup_windows(platform_state *platform.PlatformState, app_name string, x, y, width, height int32) error {
 	log.Debug("Windows Startup")
-	// load_windows_api()
 
 	internal_state := &internal_state_windows{}
 	platform_state.Internal_state = internal_state
@@ -35,25 +29,5 @@ func Platform_startup_windows(platform_state *platform.PlatformState, app_name s
 	}
 	log.Debug("handle=%#v", handle)
 
-	// unload_windows_api()
 	return nil
 }
-
-// func load_windows_api() {
-// 	var err error
-// 	_kernel32DLL, err = windows.LoadLibrary("kernel32.dll")
-// 	if err != nil {
-// 		log.Error("Cannot load kernel32.dll")
-// 	}
-
-// 	_GetModuleHandleA, err = windows.GetProcAddress(_kernel32DLL, "GetModuleHandleA")
-// 	if err != nil {
-// 		log.Error("Cannot load `GetModuleHandleA`")
-// 	}
-// }
-// func unload_windows_api() {
-// 	err := windows.FreeLibrary(_kernel32DLL)
-// 	if err != nil {
-// 		log.Error("Couldn't free kernel32")
-// 	}
-// }
